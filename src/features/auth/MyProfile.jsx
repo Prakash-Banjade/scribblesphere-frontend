4d0cc2365b35606974cd5ce79cb2b59a253dad4c
Prakash Banjade <prakashbanjade191@gmail.com>
2023-06-26 19:20:00 +0545

    worked on my profile section, editing form

--- src/features/auth/MyProfile.jsx
+++ src/features/auth/MyProfile.jsx
@@ -8,35 +8,25 @@ import Loader from "../../components/Loader";
 
 import Button from "@mui/material/Button";
 import EditIcon from "@mui/icons-material/Edit";
+import EditForm from "./ProfileEditingForm";
 
 const MyProfile = () => {
   const name = useSelector(selectCurrentUser);
 
   const [detailState, setDetailState] = useState({});
-  const [bio, setBio] = useState("");
 
   const [isEditing, setIsEditing] = useState(false);
 
-  const [qualificationEdit, setQualificationEdit] = useState("");
-  const [writesOnEdit, setWritesOnEdit] = useState("");
-  const [socialLinksEdit, setSocialLinksEdit] = useState([]);
-
-  const [addressEdit, setAddressEdit] = useState("");
 
   const myDetails = useGetMyDetailsQuery();
 
   useEffect(() => {
     if (!myDetails.isLoading) {
       setDetailState(myDetails?.data?.details);
-      // console.log(myDetails.data.details)
     }
   }, [myDetails.isLoading]);
 
   if (myDetails.isLoading) return <Loader />;
-
-  const handleSaveEdit = (e) => {
-    e.preventDefault();
-  };
 
   const WritesOn = ({ tags }) => {
     return (
@@ -48,52 +38,7 @@ const MyProfile = () => {
     );
   };
 
-  const EditForm = () => {
-    <form onSubmit={handleSaveEdit}>
-      <div className="form-field">
-        <label htmlFor="bio">Bio:</label>
-        <input
-          type="text"
-          id="bio"
-          value={bio}
-          onChange={(e) => setBio(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="address">address:</label>
-        <input
-          type="text"
-          id="address"
-          value={addressEdit}
-          onChange={(e) => setAddressEdit(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="qualification">Bio:</label>
-        <input
-          type="text"
-          id="qualification"
-          value={qualificationEdit}
-          onChange={(e) => setQualificationEdit(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="writesOn">Bio:</label>
-        <input
-          type="text"
-          id="writesOn"
-          value={writesOnEdit}
-          onChange={(e) => setWritesOnEdit(e.target.value)}
-        />
-      </div>
-
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: "var(--primary-color)" }}
-        type="submit"
-      ></Button>
-    </form>;
-  };
+  
 
   return (
     <main className="myProfile-main">
@@ -107,23 +52,27 @@ const MyProfile = () => {
 
       <div className="details">
         <header className="heading flex justify-between align-center">
-          <h3 className="font-primary color-text-white fw-500">Details</h3>
-          <Button
-            color="primary"
-            sx={{ fontSize: "1rem", display: "flex", alignItems: "center" }}
-            onClick={() => {
-              setIsEditing(true);
-            }}
-          >
-            <EditIcon />
-            Edit
-          </Button>
+          <h3 className="font-primary color-text-white fw-500">
+            {isEditing ? "Edit you details here" : "Details"}
+          </h3>
+          {!isEditing && (
+            <Button
+              color="primary"
+              sx={{ fontSize: "1rem", display: "flex", alignItems: "center" }}
+              onClick={() => {
+                setIsEditing(true);
+              }}
+            >
+              <EditIcon />
+              Edit
+            </Button>
+          )}
         </header>
 
         <hr />
 
         {isEditing ? (
-          <EditForm />
+          <EditForm details={detailState} />
         ) : (
           <ul>
             <li>{name}</li>
---
+++ src/features/auth/ProfileEditingForm.jsx
@@ -0,0 +1,84 @@
+import React, {useState} from "react";
+
+import Button from "@mui/material/Button";
+
+const EditForm = ({details}) => {
+
+  const [qualification, setQualification] = useState(details.qualification);
+  const [writesOn, setWritesOn] = useState(details.writesOn.join(','));
+//   const [socialLinks, setSocialLinks] = useState(details.socialLinks.join(','));
+
+  const [address, setAddress] = useState(details.address);
+  const [bio, setBio] = useState(details.bio);
+  const [nickname, setNickname] = useState(details.nickname);
+
+
+  const handleSaveEdit = (e) => {
+    e.preventDefault();
+  };
+  
+  return (
+    <form
+      onSubmit={handleSaveEdit}
+      className="details-editing-form flex flex-column g-20"
+    >
+      <div className="form-field">
+        <label htmlFor="bio">Tell something about you</label>
+        <textarea
+          id="bio"
+          rows="2"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="nickname">A nickname for you</label>
+        <input
+          type="text"
+          id="nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="qualification">Qualification</label>
+        <input
+          type="text"
+          id="qualification"
+          value={qualification}
+          onChange={(e) => setQualification(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="writesOn">
+          Mention what you write on (comma separated)
+        </label>
+        <input
+          type="text"
+          id="writesOn"
+          value={writesOn}
+          onChange={(e) => setWritesOn(e.target.value)}
+        />
+      </div>
+
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "var(--primary-color)" }}
+        type="submit"
+      >
+        Save changes
+      </Button>
+    </form>
+  );
+};
+
+export default EditForm;
